00d88268581f37b56fd00c3b09d533400a27788b
Benni0109 <ben2004engler@gmail.com>
2021-06-18 13:51:07 +0200

    implementing cat and restart command

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ client.on('message', message => {
 	}
 
 	try {
-		command.execute(message, args);
+		command.execute(message, args, client);
 	} catch (error) {
 		console.error(error);
 		message.reply('There was an issue executing that command!');
