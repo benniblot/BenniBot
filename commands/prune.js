dca4b0d6332e23cea678a7110b782910313fa218
Benni0109 <ben2004engler@gmail.com>
2021-09-19 19:41:43 +0200

    fixed the number of messages that get deleted

--- commands/prune.js
+++ commands/prune.js
@@ -9,7 +9,7 @@ module.exports = {
 				.setDescription('Number of Messages')
 				.setRequired(true)),
 	async execute(interaction) {;
-		const amount = parseInt(interaction.options.getInteger('number')) + 1;
+		const amount = parseInt(interaction.options.getInteger('number'));
 
 		if (amount <= 1 || amount > 100) {
 			interaction.reply({ content: 'You need to input a number between 1 and 99.', allowedMentions: { repliedUser: true } });
@@ -19,6 +19,6 @@ module.exports = {
 			console.error(err);
 			interaction.reply({ content: 'There was an error trying to prune messages in this channel!', allowedMentions: { repliedUser: true } });
 		});
-		interaction.reply({ content: amount-1 + ' Messages got deleted', allowedMentions: { repliedUser: true } });
+		interaction.reply({ content: amount + ' Messages got deleted', allowedMentions: { repliedUser: true } });
 	},
 };
