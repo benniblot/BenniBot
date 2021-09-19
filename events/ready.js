3262290ca48f1f01ea84f148d82dfcb85a11f9f9
Benni0109 <ben2004engler@gmail.com>
2021-09-19 19:48:00 +0200

    added Version Explaination

--- events/ready.js
+++ events/ready.js
@@ -8,5 +8,12 @@ module.exports = {
 			type: 'PLAYING',
 		});
 		console.log(`${client.user.tag}` + ' Version ' + bot_info.version + ' started sucessfully!');
+		/*
+		Version INFO: <Major.Minor.Revision.Build>
+		- Major is a major update to the software
+		- Minor is a small update to the software
+		- Revision is any change made (bug fixes, small updates)
+		- Build number (normally an auto increment if used)
+		*/
 	},
 };
