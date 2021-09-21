0e8ab0a36e19a1624ad3fc24cba71b1fe2050bb5
Benni0109 <ben2004engler@gmail.com>
2021-09-21 11:31:19 +0200

    update

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { } = require('./config.json');
 require('dotenv').config({
     path: '.env',
 });
-
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 
 client.commands = new Collection();
